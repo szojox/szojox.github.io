c0b447fe59fcb0ea2702ae6f8de7d5d396662a20
Szojox <mix974.kamil.szopinski@gmail.com>
2022-12-08 22:48:17 +0100

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -36,9 +36,9 @@ $('#cmd').css('left',parseInt($('#cmd').innerWidth())/3);
 setTimeout(function(){$("#cmdlist").append(  '<div>Type help to get more info</div>')}, 700);  
 $("#cmdinput").on("keydown",function search(e) {
   if(e.keyCode == 13) {
+   
     if($('#cmdinput').val().toLowerCase() == 'cv'){
-      $("#cmdlist").append('<div>guest@szojox.github.io# '+$(this).val()+'</div>');
-      $.get("cv.html", function(data){
+       $.get("cv.html", function(data){
         $('body').append(data);
         initDragElement();
         $('#cv').css('top',parseInt($('#cv').innerHeight())/4);
@@ -48,15 +48,17 @@ $("#cmdinput").on("keydown",function search(e) {
     });
 
     }
+   
     if($('#cmdinput').val().toLowerCase() == 'cv-download'){
     window.location.href = "cv.html";}
       if($('#cmdinput').val().toLowerCase() == 'help'){
-        $("#cmdlist").append('<div>guest@szojox.github.io# '+$(this).val()+'</div>');
         setTimeout(function(){$("#cmdlist").append(  '<div>cv - view cv in cmd</div>')}, 250);  
         setTimeout(function(){$("#cmdlist").append(  '<div>cv-download - download cv in pdf file</div>')}, 550);  
 
-      }else{
-      $("#cmdlist").append('<div>guest@szojox.github.io# '+$(this).val()+'</div>');}
+      }
+      
+     
+      $("#cmdlist").append('<div>guest@szojox.github.io# '+$(this).val()+'</div>');
       $(this).val('');
   }
 });
